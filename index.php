7bb73562d351e2d11fbda6ef0ed2d7950fb6feb1
MrAnyx <1ptitigre@gmail.com>
2019-12-05 21:20:37 +0100

    [add] base

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php 
-
-require __DIR__ . "/vendor/autoload.php";
-
-$router = new AltoRouter();
-
-$router->map( 'GET', '/', function() {
-    require __DIR__ . '/views/home.php';
-});
-
+<?php require __DIR__ . "/views/main.php"; ?>
---
+++ views/main.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Site de l'équipe Crab'Gang++ à la nuit de l'info 2019">
+	<meta name="keywords" content="HTML, CSS, JAVASCRIPT, NUIT INFO, 2019">
+	<meta name="author" content="Crab'Gang++">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	
+	<!-- Fonts -->
+
+	<!-- Personal -->
+	<link rel="stylesheet" href="../src/style.css">
+
+	<!-- Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	
+	
+	<!-- Personal -->
+	<script src="../src/script.js"></script>
+
+	<!-- Icone -->
+	<link rel="icon" href="#" />
+	
+	<title>Nuit de l'info 2019</title>
+
+</head>
+<body>
+
+	<header>
+		<div id="content_header" class="">
+			<div id="logo" class="">
+				<img src="../assets/logo.png" alt="Logo de la page">
+			</div>
+			<div id="links" class="">
+				<a href="#" class="link">Features</a>
+				<a href="#" class="link">Feedback</a>
+				<a href="#" id="dl_app">Download app</a>
+			</div>
+		</div>
+	</header>
+
+	<main>
+		<div id="image_header">
+			<img src="../assets/image.jpg" alt="main image header">
+			<div id="caption_image">
+				<h1>Nuit de l'info 2019</h1>
+				<h2>Crab'Gang++</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+				<div id="buttons_image">
+					<a href="#">Previous project</a>
+					<a href="#">► Watch a video</a>
+				</div>
+			</div>
+		</div>
+		<h1 id="title_content">Big plus to your productivity</h1>
+
+		<div class="container-fluid pt-4 mb-5">
+			<div class="row justify-content-center">
+				<div class="col-lg-6 col-xl-4 col-md-8 col-11 mr-5 card">
+					<div class="card-content">
+						<div class="decoration"></div>
+						<div class="content">
+							<span class="date">28 August 2019</span>
+							<h6 class="title pt-1">Lorem Ipsum Dolor</h6>
+							<div class="resume">Dolore veniam sit sunt culpa commodo officia dolor adipisicing dolore esse sint exercitation velit ut veniam in ad quis ut sunt sit elit eiusmod in eu laboris.</div>
+							<br>
+							<a href="#" class="read-more px-4 py-2">Read More</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-xl-4 col-md-8 col-11 card ml-5">
+					<div class="card-content">
+						<div class="decoration"></div>
+						<div class="content">
+							<span class="date">28 August 2019</span>
+							<h6 class="title pt-1">Lorem Ipsum Dolor</h6>
+							<div class="resume">Dolore veniam sit sunt culpa commodo officia dolor adipisicing dolore esse sint exercitation velit ut veniam in ad quis ut sunt sit elit eiusmod in eu laboris.</div>
+							<br>
+							<a href="#" class="read-more px-4 py-2">Read More</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="row justify-content-center mt-5">
+				<div class="col-lg-6 col-xl-4 col-md-8 col-11 mr-5 card">
+					<div class="card-content">
+						<div class="decoration"></div>
+						<div class="content">
+							<span class="date">28 August 2019</span>
+							<h6 class="title pt-1">Lorem Ipsum Dolor</h6>
+							<div class="resume">Dolore veniam sit sunt culpa commodo officia dolor adipisicing dolore esse sint exercitation velit ut veniam in ad quis ut sunt sit elit eiusmod in eu laboris.</div>
+							<br>
+							<a href="#" class="read-more px-4 py-2">Read More</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-xl-4 col-md-8 col-11 card ml-5">
+					<div class="card-content">
+						<div class="decoration"></div>
+						<div class="content">
+							<span class="date">28 August 2019</span>
+							<h6 class="title pt-1">Lorem Ipsum Dolor</h6>
+							<div class="resume">Dolore veniam sit sunt culpa commodo officia dolor adipisicing dolore esse sint exercitation velit ut veniam in ad quis ut sunt sit elit eiusmod in eu laboris.</div>
+							<br>
+							<a href="#" class="read-more px-4 py-2">Read More</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+	</main>
+
+	<hr>
+
+	<footer>
+		<div id="footer_color">
+			<div id="text_footer">
+				<h1>Lorem ipsum dolor sit amet, consectetur adipisicing sunt</h1>
+				<div id="buttons_image" class="mt-4">
+					<a href="#">Previous project</a>
+					<a href="#">► Watch a video</a>
+				</div>
+			</div>
+		</div>
+
+		<div id="footer">
+			<div class="footer_flex">
+				© Crab'Gang
+			</div>
+			<div class="footer_flex">
+				<div id="logo_footer" class="mx-auto">
+					<img src="../assets/logo.png" alt="logo footers">
+				</div>
+			</div>
+
+			<div class="footer_flex text-right">
+				<a href="#">Privacy</a>
+			</div>
+		</div>
+		
+	</footer>
+
+</body>
+</html>	
