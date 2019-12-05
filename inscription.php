c6d935049a62fe36b1518b6e03095580ba77c5b6
Marmorand <martinmorand31@gmail.com>
2019-12-05 23:39:25 +0100

    inscription form

---
+++ inscription.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Description">
+	<meta name="keywords" content="HTML, CSS, JAVASCRIPT">
+	<meta name="author" content="Robin Bidanchon">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Clarity CSS -->
+	<link rel="stylesheet" href="https://unpkg.com/@clr/ui/clr-ui.min.css" />
+
+	<title>Inscription</title>
+
+</head>
+<body>
+  <form clrForm clrLayout="Horizontal">
+    <div>
+      <clr-input-container>
+        <label>My name</label>
+        <div class="clr-control-container">
+          <div class="clr-input-wrapper">
+            <input clrinput="" name="input" placeholder="My input" class="clr-input ng-pristine ng-valid ng-touched" id="clr-form-control-191">
+          </div>
+        </div>
+      </clr-input-container>
+    </div>
+    <div>
+      <clr-input-container>
+        <label>My name</label>
+        <div class="clr-control-container">
+          <div class="clr-input-wrapper">
+            <input clrinput="" name="input" placeholder="My input" class="clr-input ng-pristine ng-valid ng-touched" id="clr-form-control-191">
+          </div>
+        </div>
+      </clr-input-container>
+    </div>
+</form>
+</body>
+</html>
